d39250c190ed87bb64584a2f70cd9f012945502e
rjmax13 <rjmax13@yahoo.com>
2026-04-30 17:00:31 -0400

    updated sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,4 +1,10 @@
+import requests #Import requests library to handle HTTP requests
+
 # analysis function
-def sentiment_analyzer(text);
-    text_to_analyse = text
-    return text
+def sentiment_analyzer(text_to_analyse);
+# Define a function named sentiment_analyzer that takes a string
+    input(text_to_analyse) url= 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    headers = {<header_dictionary>}
+	myobj = {"raw_document": { "text": text_to_analyse }}
+	response = requests.post(url, json = myobj, headers=header)
+    return response.text
