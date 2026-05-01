8083cb06514e0a74060221ebb3304c177de44545
rjmax13 <rjmax13@yahoo.com>
2026-05-01 11:04:05 -0400

    completed sentiment_analyzer function

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,10 +1,16 @@
-import requests #Import requests library to handle HTTP requests
+import requests, json#Import requests library to handle HTTP requests
 
 # analysis function
-def sentiment_analyzer(text_to_analyse);
+def sentiment_analyzer(text_to_analyse):
 # Define a function named sentiment_analyzer that takes a string
-    input(text_to_analyse) url= 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-    headers = {<header_dictionary>}
-	myobj = {"raw_document": { "text": text_to_analyse }}
-	response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    url= 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    myobj = {"raw_document": { "text": text_to_analyse }}
+    response = requests.post(url, json = myobj, headers=headers)
+    #Parsing JSON response from the API formatted_response
+    formatted_response = json.loads(response.text)
+    # Extracting sentiment label and score from the response
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
+
+    return {'label': label, 'score': score}
